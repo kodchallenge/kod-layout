5b146b3f8b447124863d0dec8c9a27d4b2cdab5c
Yasin Torun <yasintorun4680@gmail.com>
2022-10-21 19:43:56 +0300

    Added light theme

--- src/theme/theme-context.ts
+++ src/theme/theme-context.ts
@@ -1,9 +1,11 @@
+import { LightTheme } from './lightTheme';
 import React from 'react';
 import { DarkTheme } from '../theme/darkTheme';
 import { Theme } from '../theme/theme';
 
 export const themes = {
   dark: DarkTheme,
+  light: LightTheme
 }
 
 export const ThemeContext = React.createContext(themes.dark);
