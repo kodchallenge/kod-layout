205bfb9c1ae8c9957b40254b94e088e1e396784a
Yasin Torun <yasintorun4680@gmail.com>
2022-10-21 19:41:57 +0300

    Create column - added splitter

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Split from "react-split"
 
 export type ColumnProps = {
-
+  gutterSize?: number;
+  snapOffset?: number;
 }
-const Column: React.FC<ColumnProps & React.HTMLAttributes<HTMLDivElement>> = ({...props}) => {
-  const {children, ...other} = props
+const Column: React.FC<ColumnProps & React.HTMLAttributes<HTMLDivElement>> = ({ ...props }) => {
+  const { children, gutterSize, snapOffset, className, ...other } = props
   return (
-    <div {...other}>
-      {children}
+    <div className={`kl-column ${className ?? ""}`} {...other}>
+      <Split
+				direction="vertical"
+        className="kl-split"
+        gutterSize={gutterSize ?? 3}
+        snapOffset={snapOffset ?? 0}
+      >
+        {children}
+      </Split>
     </div>
   )
 }
