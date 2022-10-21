cf88834e5fdbbf49f5b1efacd70eafa4d7c9267a
Yasin Torun <yasintorun4680@gmail.com>
2022-10-21 19:42:10 +0300

    Create row - added splitter

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-export type RowProps = {
+import Split from "react-split"
 
+export type RowProps = {
+  snapOffset?: number;
+  gutterSize?: number;
+  maxSize?: number | number[];
+  minSize?: number | number[];
+  sizes?: number[]
 }
-const Row: React.FC<RowProps & React.HTMLAttributes<HTMLDivElement>> = ({...props}) => {
-  const {children, ...other} = props
+const Row: React.FC<RowProps & React.HTMLAttributes<HTMLDivElement>> = ({ ...props }) => {
+  const { children, snapOffset, minSize, maxSize, sizes, gutterSize, className, ...other } = props
+  
   return (
-    <div {...other}>
-      {children}
+    <div className={`kl-row ${className ?? ""}`} {...other}>
+      <Split
+        className="kl-split"
+        gutterSize={gutterSize ?? 3}
+        minSize={minSize}
+        maxSize={maxSize}
+        sizes={sizes}
+        snapOffset={snapOffset ?? 0}
+      >
+        {children}
+      </Split>
     </div>
   )
 }
